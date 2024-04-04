108a5eb05f138f467035706c600dfbb7686a15d9
xmatyas <adam.matyas2@gmail.com>
2024-04-04 12:53:42 +0200

    bump

--- machines/skadi/filesystems/disko.nix
+++ machines/skadi/filesystems/disko.nix
@@ -1,79 +1,74 @@
-{
-  device ? throw "You need to specify a device to install to using --arg device '"/dev/vda"'",
-  ...
-}: {
-  disko.devices = {
-    disk.main = {
-      inherit device;
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
+disko.devices = {
+  disk.main = {
+    device = "CHANGEDISK";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          name = "boot";
+          size = "1M";
+          type = "EF02";
+        };
+        esp = {
+          name = "ESP";
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "defaults" ];
           };
-          esp = {
-            name = "ESP";
-            size = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "defaults" ];
-            };
+        };
+        swap = {
+          size = "32G";
+          content = {
+            type = "swap";
+            resumeDevice = true;
           };
-          swap = {
-            size = "32G";
-            content = {
-              type = "swap";
-              resumeDevice = true;
-            };
-          };
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "root_vg";
-            };
+        };
+        root = {
+          name = "root";
+          size = "100%";
+          content = {
+            type = "lvm_pv";
+            vg = "root_vg";
           };
         };
       };
     };
-    lvm_vg = {
-      root_vg = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "btrfs";
-              extraArgs = ["-f"];
+  };
+  lvm_vg = {
+    root_vg = {
+      type = "lvm_vg";
+      lvs = {
+        root = {
+          size = "100%FREE";
+          content = {
+            type = "btrfs";
+            extraArgs = ["-f"];
 
-              subvolumes = {
-                "/root" = {
-                  mountpoint = "/";
-                  mountOptions = ["subvol=root" "noatime" "compress=zstd"];
-                };
-                "/home" = {
-                  mountOptions = ["subvol=home" "noatime" "compress=zstd"];
-                  mountpoint = "/home";
-                };
-                "/persist" = {
-                  mountOptions = ["subvol=persist" "noatime" "compress=zstd"];
-                  mountpoint = "/persist";
-                };
-                "/nix" = {
-                  mountOptions = ["subvol=nix" "noatime" "compress=zstd"];
-                  mountpoint = "/nix";
-                };
-                "/var/log" = {
-                  mountOptions = ["subvol=log" "noatime" "compress=zstd"];
-                  mountpoint = "/var/log";
-                };
+            subvolumes = {
+              "/root" = {
+                mountpoint = "/";
+                mountOptions = ["subvol=root" "noatime" "compress=zstd"];
+              };
+              "/home" = {
+                mountOptions = ["subvol=home" "noatime" "compress=zstd"];
+                mountpoint = "/home";
+              };
+              "/persist" = {
+                mountOptions = ["subvol=persist" "noatime" "compress=zstd"];
+                mountpoint = "/persist";
+              };
+              "/nix" = {
+                mountOptions = ["subvol=nix" "noatime" "compress=zstd"];
+                mountpoint = "/nix";
+              };
+              "/var/log" = {
+                mountOptions = ["subvol=log" "noatime" "compress=zstd"];
+                mountpoint = "/var/log";
               };
             };
           };
@@ -81,4 +76,4 @@
       };
     };
   };
-}
+};
