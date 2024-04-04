d65f55760379434153942d5e575a8b2efa3f42a1
xmatyas <adam.matyas2@gmail.com>
2024-04-04 13:00:00 +0200

    fix: disko.nix as module structure

--- machines/skadi/filesystems/disko.nix
+++ machines/skadi/filesystems/disko.nix
@@ -1,74 +1,76 @@
-disko.devices = {
-  disk.main = {
-    device = "CHANGEDISK";
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        boot = {
-          name = "boot";
-          size = "1M";
-          type = "EF02";
-        };
-        esp = {
-          name = "ESP";
-          size = "512M";
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [ "defaults" ];
+{
+  disko.devices = {
+    disk.main = {
+      device = "CHANGEDISK";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
           };
-        };
-        swap = {
-          size = "32G";
-          content = {
-            type = "swap";
-            resumeDevice = true;
+          esp = {
+            name = "ESP";
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "defaults" ];
+            };
           };
-        };
-        root = {
-          name = "root";
-          size = "100%";
-          content = {
-            type = "lvm_pv";
-            vg = "root_vg";
+          swap = {
+            size = "32G";
+            content = {
+              type = "swap";
+              resumeDevice = true;
+            };
+          };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "root_vg";
+            };
           };
         };
       };
     };
-  };
-  lvm_vg = {
-    root_vg = {
-      type = "lvm_vg";
-      lvs = {
-        root = {
-          size = "100%FREE";
-          content = {
-            type = "btrfs";
-            extraArgs = ["-f"];
+    lvm_vg = {
+      root_vg = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "btrfs";
+              extraArgs = ["-f"];
 
-            subvolumes = {
-              "/root" = {
-                mountpoint = "/";
-                mountOptions = ["subvol=root" "noatime" "compress=zstd"];
-              };
-              "/home" = {
-                mountOptions = ["subvol=home" "noatime" "compress=zstd"];
-                mountpoint = "/home";
-              };
-              "/persist" = {
-                mountOptions = ["subvol=persist" "noatime" "compress=zstd"];
-                mountpoint = "/persist";
-              };
-              "/nix" = {
-                mountOptions = ["subvol=nix" "noatime" "compress=zstd"];
-                mountpoint = "/nix";
-              };
-              "/var/log" = {
-                mountOptions = ["subvol=log" "noatime" "compress=zstd"];
-                mountpoint = "/var/log";
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                  mountOptions = ["subvol=root" "noatime" "compress=zstd"];
+                };
+                "/home" = {
+                  mountOptions = ["subvol=home" "noatime" "compress=zstd"];
+                  mountpoint = "/home";
+                };
+                "/persist" = {
+                  mountOptions = ["subvol=persist" "noatime" "compress=zstd"];
+                  mountpoint = "/persist";
+                };
+                "/nix" = {
+                  mountOptions = ["subvol=nix" "noatime" "compress=zstd"];
+                  mountpoint = "/nix";
+                };
+                "/var/log" = {
+                  mountOptions = ["subvol=log" "noatime" "compress=zstd"];
+                  mountpoint = "/var/log";
+                };
               };
             };
           };
@@ -76,4 +78,4 @@ disko.devices = {
       };
     };
   };
-};
+}
